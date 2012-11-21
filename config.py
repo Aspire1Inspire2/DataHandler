402efa1e8fd1860c5ec7670bc4bff994cabf1eeb
Tero Suhonen <tero.suhonen@luukku.com>
2012-11-21 12:45:02 +0200

    Config file added

---
+++ config.py
@@ -0,0 +1,10 @@
+#the opening time of the day
+START_TIME = '0931'
+#the closing time of the day
+END_TIME = '1600'
+#time format of the file
+TIME_FORMAT = '%H%M'
+#time gap delta
+TIME_DELTA = '01'
+#date format of the file
+DATE_FORMAT = '%m/%d/%Y'
